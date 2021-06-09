c27df21af5a98568ca4ccc53784083d9859e2cff
Aromal S <aromalsanthosh@protonmail.com>
2021-06-09 08:41:50 +0530

    nothing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '../Header/Header';
-import './App.css'
+import './App.css';
+
+
 
 class App extends React.Component{
+  state = {
+    headerText:'Hello User'
+  }
+
   render(){
     return(
       <div>
         <Header/>
+        <h3>{this.state.headerText}</h3>
+        <button 
+        onClick={()=> {
+          this.setState({headerText:"Wow"})
+          console.log(this.state.headerText);
+        }}>Magic HAppens Here!</button>
       </div>
     )
   }
