001d01b9266cc136f1fb956e207686b12356373a
Aromal S <aromalsanthosh@protonmail.com>
2021-06-09 08:10:25 +0530

    header-component-done🌄

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 const Header =() =>{
     return(
         <div className="head-container">
+            <img 
+                src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png" 
+                alt="icon-header" 
+                className="head-image" 
+            />
             <h1 className="head-text">Name It!</h1>
         </div>
     )
